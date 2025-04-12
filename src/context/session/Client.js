7cf1c51f67ea7d723b346132c2ae0b11b78d107f
ajitaabhardwaj <44480136+ajitaabhardwaj@users.noreply.github.com>
2025-04-13 02:08:26 +0530

    bug fixes

--- src/context/session/Client.js
+++ src/context/session/Client.js
@@ -24,8 +24,8 @@ class AfoozoClient {
   constructor() {
     this.session = null;
     this.socket = null;
-    // this.mockup = 'https://api.afoozo.com';
-    this.mockup = 'http://localhost:5000';
+    this.mockup = 'https://api.afoozo.com';
+    // this.mockup = 'http://localhost:5000';
     // List of AfoozoMonitors.
     this.monitors = [];
 
